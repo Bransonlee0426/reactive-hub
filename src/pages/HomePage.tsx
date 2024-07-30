c44be5a13f0e6fa718670c5f911c40e9f50ec922
Branson <bransonlee0426@gmail.com>
2024-07-30 17:21:51 +0800

    change react-transition-group use way

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,21 +1,47 @@
+import { useRef } from 'react';
 import Books from '../components/Books';
 import NavBar from '../components/NavigationBar';
 import Banner from '../components/Banner';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
+
+function HomePage() {
+  const location = useLocation();
+  const nodeRef = useRef(null);
+  return (
+    <div className="h-screen">
+      <PageContainer>
+        <NavBar />
+        <SwitchTransition>
+          <CSSTransition key={location.pathname} nodeRef={nodeRef} classNames="fade" timeout={200}>
+            <StyledContent ref={nodeRef}>
+              <Routes location={location}>
+                <Route path="/" element={<Banner />} />
+                <Route path="/books" element={<Books />} />
+              </Routes>
+            </StyledContent>
+          </CSSTransition>
+        </SwitchTransition>
+      </PageContainer>
+    </div>
+  );
+}
+
+export default HomePage;
 
 const PageContainer = styled.div`
   height: 100vh;
 `;
 
 const StyledContent = styled.div`
+  height: calc(100vh - 10px);
   &.fade-enter {
     opacity: 0;
   }
   &.fade-enter-active {
     opacity: 1;
-    transition: opacity 360ms ease-in;
+    transition: opacity 200ms ease-in;
   }
   &.fade-exit {
     opacity: 1;
@@ -25,25 +51,3 @@ const StyledContent = styled.div`
     transition: opacity ease-in;
   }
 `;
-function HomePage() {
-  const location = useLocation();
-  return (
-    <div className="h-screen">
-      <PageContainer>
-        <NavBar />
-        <TransitionGroup>
-          <CSSTransition key={location.pathname} classNames="fade" timeout={360}>
-            <StyledContent>
-              <Routes location={location}>
-                <Route path="/" element={<Banner />} />
-                <Route path="/books" element={<Books />} />
-              </Routes>
-            </StyledContent>
-          </CSSTransition>
-        </TransitionGroup>
-      </PageContainer>
-    </div>
-  );
-}
-
-export default HomePage;
